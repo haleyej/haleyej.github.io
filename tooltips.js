4414da6d3a7956158307235db75d354e2db3e902
haleyej <haleyej@umich.edu>
2024-12-25 18:58:40 -0500

    THE HATERS SAID IT WOULD NEVER WOKR

---
+++ tooltips.js
@@ -0,0 +1,11 @@
+var tooltip = document.querySelectorAll('.tooltip');
+console.log(tooltip);
+
+document.addEventListener('mousemove', fn, false);
+
+function fn(e) {
+    for (var i=tooltip.length; i--;) {
+        tooltip[i].style.left = e.pageX + 'px';
+        tooltip[i].style.top = e.pageY + 'px';
+    }
+}
