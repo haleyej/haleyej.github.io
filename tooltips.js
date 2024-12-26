f71f307d59d38c180370b39ea42f6ae11dc95414
haleyej <haleyej@umich.edu>
2024-12-25 19:16:30 -0500

    debugging

--- tooltips.js
+++ tooltips.js
@@ -2,10 +2,11 @@ var tooltip = document.querySelectorAll('.tooltip');
 console.log(tooltip);
 
 document.addEventListener('mousemove', fn, false);
+console.log('event');
 
 function fn(e) {
+    console.log('updating position');
     for (var i=tooltip.length; i--;) {
-        console.log('hover!!!')
         tooltip[i].style.left = e.pageX + 'px';
         tooltip[i].style.top = e.pageY + 'px';
     }
