751bf93bd1edf5356deb09f1d50d510ddbe1bf26
haleyej <haleyej@umich.edu>
2024-12-26 18:33:24 -0500

    clean up extraneous consoe logs

--- tooltips.js
+++ tooltips.js
@@ -4,7 +4,6 @@ console.log(tooltip);
 document.addEventListener('mousemove', fn);
 // document.onmousemove = fn;
 function fn(e) {
-    console.log('mouse move');
     for (var i=tooltip.length; i--;) {
         tooltip[i].style.left = e.pageX + 'px';
         tooltip[i].style.top = e.pageY + 'px';
